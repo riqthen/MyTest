4dac18d6bc2773756b4c6462d86981fd35b50c66
riqthen <137227739@qq.com>
2017-05-11 20:45:39 +0800

    Initial commit

---
+++ app/src/main/java/com/riq/mytest/MainActivity.java
@@ -0,0 +1,29 @@
+package com.riq.mytest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.riq.utilslib.utils.ToastUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv)
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tv)
+    public void onViewClicked() {
+        ToastUtils.showToast(this,"ssss");
+    }
+}
