3d1de0a3161de007666788d4cf5e04cb81572636
riqthen <137227739@qq.com>
2017-05-22 10:12:36 +0800

    整合CenterView点击事件

--- Test/src/main/java/com/riq/test/MainActivity.java
+++ Test/src/main/java/com/riq/test/MainActivity.java
@@ -6,7 +6,6 @@ import android.view.View;
 
 import com.riq.basebottomtablib.BaseBottomTabActivity;
 import com.riq.basebottomtablib.BottomTabView;
-import com.riq.mylibrary.utils.Lcat;
 import com.riq.mylibrary.utils.ToastUtils;
 
 import java.util.ArrayList;
@@ -17,15 +16,12 @@ public class MainActivity extends BaseBottomTabActivity {
     @Override
     protected List<BottomTabView.TabItemView> getTabViews() {
         List<BottomTabView.TabItemView> tabItemViews = new ArrayList<>();
-        tabItemViews.add(new BottomTabView.TabItemView(this, "hello", 10, 10, 10, 10
-                , Color.rgb(255, 255, 255), Color.RED, 0, 0));
+        tabItemViews.add(new BottomTabView.TabItemView(this, "", 10, 10, 10, 10
+                , 255, Color.rgb(255, 255, 0), R.mipmap.eye_close, R.mipmap.eye_open));
         tabItemViews.add(new BottomTabView.TabItemView(this, "h2", 10, 10, 10, 10
-                , 0, Color.argb(55,44,51,47), R.mipmap.eye_close, R.mipmap.eye_open));
-        Lcat.print(String.valueOf(Color.BLUE));
-        Lcat.print(String.valueOf(R.color.colorPrimary));
-        Lcat.print(String.valueOf("#124"));
-        Lcat.print(String.valueOf(Color.rgb(255, 255, 255)));
-        Lcat.print(String.valueOf(Color.argb(55,44,51,47)));
+                , 0, Color.argb(255, 0, 0, 255), 0, R.mipmap.eye_open));
+        tabItemViews.add(new BottomTabView.TabItemView(this, "h2", 10, 10, 10, 10
+                , 0, Color.argb(55, 44, 51, 47), R.mipmap.eye_close, R.mipmap.eye_open));
         return tabItemViews;
     }
 
@@ -34,23 +30,16 @@ public class MainActivity extends BaseBottomTabActivity {
         List<Fragment> fragments = new ArrayList<>();
         fragments.add(new FragmentTab1());
         fragments.add(new FragmentTab2());
+        fragments.add(new FragmentTab1());
         return fragments;
     }
 
     @Override
-    public View setCenterView(int iconRes, int iconWidth, int iconHeight, int leftMargin, int rightMargin, boolean isOutTab) {
-        return super.setCenterView(R.mipmap.ic_launcher_round, 300, 300, 10, 10, true);
+    public View setCenterView(int iconRes, int iconWidth, int iconHeight, int leftMargin, int rightMargin, boolean isOutTab, View.OnClickListener onClickListener, View.OnLongClickListener onLongClickListener) {
+        return super.setCenterView(R.mipmap.ic_launcher_round, 200, 200, 20, 20, true, onClickListener, onLongClickListener);
     }
 
-    @Override
-    public void onCenterViewClick() {
-        ToastUtils.showToast(this, "点击");
-    }
 
-    @Override
-    public void onCenterViewLongClick() {
-        ToastUtils.showToast(this, "长按");
-    }
 
     @Override
     public void onSecondClick() {
