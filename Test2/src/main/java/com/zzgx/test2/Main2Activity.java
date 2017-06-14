25c299e5151cad79e1e93ee5d7bd34a34cb8108a
Dreamriq <137227739@qq.com>
2017-06-14 14:43:11 +0800

    null

--- Test2/src/main/java/com/zzgx/test2/Main2Activity.java
+++ Test2/src/main/java/com/zzgx/test2/Main2Activity.java
@@ -2,12 +2,27 @@ package com.zzgx.test2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+    private OnMyClickListener onMyClickListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        TextView tv=new TextView(this);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onMyClickListener.onClick();
+            }
+        });
+    }
+
+    public void setOnMyClickListener(OnMyClickListener onMyClickListener) {
+        this.onMyClickListener = onMyClickListener;
     }
 }
---
+++ Test2/src/main/java/com/zzgx/test2/OnMyClickListener.java
@@ -0,0 +1,9 @@
+package com.zzgx.test2;
+
+/**
+ * Created by 10903 on 2017/6/5.
+ */
+
+public interface OnMyClickListener {
+    void onClick();
+}
